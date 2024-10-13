f9659e0a886cd84256ed5abf7d19f2b5e565ddb7
Mahfuza Siddiqua <157055894+mahfuza2000@users.noreply.github.com>
2024-10-13 08:02:18 -0500

    ver3

--- App.py
+++ App.py
@@ -1,91 +1,136 @@
-import altair as alt
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import altair as alt
 
-# Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+
+st.set_page_config(page_title="Crystal Clear", page_icon="🌊")
+st.title("Crystal Clear Water Quality Tracker")
+
 st.write(
     """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
+    Investigate and compare local water quality data pulled from annual municiple water quality reports.
     """
 )
-
-
-# Load the data from a CSV. We're caching this so it doesn't reload every time the app
-# reruns (e.g. if the user interacts with the widgets).
-@st.cache_data
-def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
-    return df
-
-
-df = load_data()
-
-# Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-)
-
-# Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
-
-# Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-)
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-
-
-# Display the data as a table using `st.dataframe`.
-st.dataframe(
-    df_reshaped,
-    use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
-)
-
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
-    )
-    .properties(height=320)
-)
-st.altair_chart(chart, use_container_width=True)
-
+st.divider()
 
 @st.cache_data
 def load_data():
     df = pd.read_csv("data/WaterQualityDatabase.csv")
     return df
 
-
 df = load_data()
+df['Year'] = df['Year'].astype(str)
 
-cities = st.multiselect(
-    "Cities",
-    df.City.unique(),
-    ["Arlington", "Fort Worth", "Irving"],
-)
 
-years = st.slider("Year", 2021, 2023, (2021, 2023))
+mode = st.selectbox("Select Mode", ("Normal Mode", "Year Analysis Mode", "City Comparison Mode"))
+st.divider()
 
-# df_filtered = df[(df["City"].isin(cities)) & (df["Year"].between(years[0], years[1]))]
-# df_reshaped = df_filtered.pivot_table(
-#     index="City", columns="Year", values="gross", aggfunc="sum", fill_value=0
-# )
-# df_reshaped = df_reshaped.sort_values(by="City", ascending=False)
+if mode == "Normal Mode":
 
-st.dataframe(df)
+    years = st.slider("Year", 2021, 2023, (2021, 2023))
+
+    cities = st.multiselect(
+        "Select Cities to Display",
+        df.City.unique(),
+        ["Arlington", "Fort Worth", "Irving"],
+    )
+
+    contaminant = st.multiselect(
+        "Contaminants to Filter",
+        df.Contaminant.unique(),
+        ["Arsenic", "Atrazine", "Barium", "Bromodichloromethane", "Bromofoam", 
+        "Chloroform", "Chromiun", "Coliforms (fecal, E coli, etc)", "Copper", "Cyanide", 
+        "Dibromochloromethane", "Fluoride", "Lead", "Nitrate", "Nitrite"]
+    )
+
+    st.write("\nAdditional Options\n")
+    highlight_mcl = st.checkbox("Show Average Level > MCL Allowed", value=False)
+    highlight_mclg = st.checkbox("Show Average Level > MCLG", value=False)
+
+    if years:
+        df = df[(df['Year'].astype(int) >= years[0]) & (df['Year'].astype(int) <= years[1])]
+
+    if cities:
+        df = df[df['City'].isin(cities)]
+
+    if contaminant:
+        df = df[df['Contaminant'].isin(contaminant)]
+
+    def highlight_rows(row):
+        if highlight_mcl and row['Average Level'] > row['MCL Allowed']:
+            return ['background-color: #FF4D4D'] * len(row)
+        elif highlight_mclg and row['Average Level'] > row['MCLG']:
+            return ['background-color: #9999FF'] * len(row)
+        return [''] * len(row)
+
+
+    #st.subheader("Database of Municiple Water Quality")
+    st.text("")
+    st.markdown("<h2 style='text-align: center;'>Database of Municiple Water Quality</h2>", unsafe_allow_html=True)
+    
+    st.dataframe(df.style.apply(highlight_rows, axis=1), hide_index=True, use_container_width=True)
+    # df.style.apply(highlight_rows, axis=1)
+    # st.dataframe(df, hide_index=True, use_container_width=True)
+
+    st.caption(
+        """
+        MCL: Maximum Contaminant Level Allowed (Regulated By Federal Government)\n
+        MCLG: Maximum Contaminant Level Goal (Aspiring Target)
+        """
+    )
+
+elif mode == "Year Analysis Mode":
+
+
+    city = st.selectbox("Select City", df.City.unique())
+    contaminants = st.multiselect("Select Contaminant", df['Contaminant'].unique())
+
+    if  contaminants:
+        df_analysis = df[(df['City'] == city) & (df['Contaminant'].isin(contaminants))]
+    else:
+        df_analysis = df[df['City'] == city]  
+
+    st.text("")
+    if not df_analysis.empty:
+        chart_analysis = (
+            alt.Chart(df_analysis)
+            .mark_line()
+            .encode(
+                x=alt.X("Year:O", title="Year", axis=alt.Axis(labelAngle=0)),
+                y=alt.Y("Average Level:Q", title="Average Level (ppm/ppb)"),
+                color=alt.Color("Contaminant:N", title="Contaminant"),
+            )
+            #.properties(title=f"Change in Average Level of Contaminants in {city} by Year", height=320)
+            .properties(
+                title={"text": ["Change in Average Level of Contaminants in {city} by Year"], "anchor": "middle"},  
+                height=320
+            )
+        )
+
+        st.altair_chart(chart_analysis, use_container_width=True)
+
+elif mode == "City Comparison Mode":
+
+    year = st.selectbox("Select Year", df['Year'].unique())
+    contaminant = st.selectbox("Select Contaminant", df['Contaminant'].unique())
+
+    df_comparison = df[(df['Year'] == year) & (df['Contaminant'] == contaminant)]
+
+    st.text("")
+    if not df_comparison.empty:
+        histogram = (
+            alt.Chart(df_comparison)
+            .mark_bar()
+            .encode(
+                x=alt.X("City:N", title="City", axis=alt.Axis(labelAngle=0)),
+                y=alt.Y("Average Level:Q", title="Average Level (ppm/ppb)"),
+                color=alt.Color("City:N", title="City", legend=None),
+            )
+            #.properties(title=f"Average Level of {contaminant} in {year} by City", height=320)
+            .properties(
+                title={"text": [f"Average Level of {contaminant} in {year} by City"], "anchor": "middle"},  
+                height=320
+            )
+        )
+
+        st.altair_chart(histogram, use_container_width=True)
